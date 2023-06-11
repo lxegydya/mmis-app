1e8832ceb875f68a347fb940b1dc88dda30467d1
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2023-06-11 20:55:44 +0700

    change running port to 3000

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server:{
+		port:3000,
+		strictPort:false,
+	},
+	preview:{
+		port:3000,
+		strictPort:false,
+	},
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
